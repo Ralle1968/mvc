24a8dab3e38d9b244ffc765670ebaee59f8bdfc9
Ralle1968 <graef.ralf@gmail.com>
2017-10-06 22:48:20 +0200

    add router

---
+++ Core/Router.php
@@ -0,0 +1,10 @@
+<?php
+
+ /**
+ * Router
+ *
+ * PHP version 7.1.4
+ */
+ class Router {
+
+ }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo 'Requested URL: = "' . $_SERVER['QUERY_STRING'] . '"';
+/**
+* Front Controller
+*
+* PHP version 7.1.4
+*/
+
+// echo 'Requested URL: = "' . $_SERVER['QUERY_STRING'] . '"';
+
+/**
+* Routing
+*/
+
+require '../Core/Router.php';
+
+$router = new Router();
+
+echo get_class($router);
